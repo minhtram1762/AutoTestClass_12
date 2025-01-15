021a779c368c9b528a5e2dd776f68330b67d6fd8
Le Junnie <lejunnie@Junnies-MacBook-Air.local>
2025-01-15 22:22:45 +0800

    add new code

---
+++ GiaoVien.java
@@ -0,0 +1,15 @@
+package day5_inherittance;
+
+public class GiaoVien extends Nguoi {
+	public String monHoc;
+	
+	public void diDay()
+	{
+		System.out.println("Giao vien codestar day mon " + monHoc + " toi thu 4 va CN hang tuan");
+	}
+	
+	public void diHoc() 
+	{
+		System.out.println("di choi sau gio day");
+	}
+}
