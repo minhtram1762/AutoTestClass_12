7b1e71fbd32d98c1ee6477a6ee1ca312ec244343
Le Junnie <lejunnie@Junnies-MacBook-Air.local>
2025-02-15 21:35:46 +0800

    update folder Test project

--- Test_Project/src/main/java/automation/testsuite/LoginTest.java
+++ Test_Project/src/main/java/automation/testsuite/LoginTest.java
@@ -1,5 +1,24 @@
 package automation.testsuite;
 
-public class LoginTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.*;
 
+import automation.common.CommonBase;
+
+public class LoginTest extends CommonBase {
+	@BeforeMethod
+	public void openBrowerTest()
+	{
+		driver = initBrowser("https://alada.vn/tai-khoan/dang-nhap.html");
+	}
+	@Test
+	public void locateById()
+	{
+		WebElement textUsername = driver.findElement(By.id("txtLoginUsername"));
+		System.out.println(textUsername);
+		
+		WebElement textPassword = driver.findElement(By.id("txtLoginPassword"));
+		System.out.println("Text ID PW "+ textPassword);
+	}
 }
