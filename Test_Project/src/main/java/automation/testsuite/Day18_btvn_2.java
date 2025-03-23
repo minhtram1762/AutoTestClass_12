fc8e11ae1b60391119ca5383df9794f198ca2f21
Le Junnie <lejunnie@Junnies-MacBook-Air.local>
2025-03-23 18:19:27 +0800

    btvn buoi 18

--- Test_Project/src/main/java/automation/testsuite/Day18_btvn_2.java
+++ Test_Project/src/main/java/automation/testsuite/Day18_btvn_2.java
@@ -1,27 +1,32 @@
 package automation.testsuite;
 
 import automation.common.CommonBase;
+
+import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class Day18_btvn_1 extends CommonBase
+public class Day18_btvn_2 extends CommonBase
 {
 	@BeforeMethod
 	public void openBrowser()
 	{
-		driver = initBrowser("https://dienmaynhapkhaugiare.com.vn/");
+		driver = initBrowser("https://bepantoan.vn/");
 	}
 
 	@Test 
-	public void lienHeZalo()
+	public void lienHeMessenger()
 	{
-		WebElement zaloLink = driver.findElement(By.xpath("//div[@id='zalo-vr']"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    WebElement messengerLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//a[@href='https://m.me/1463299120624512'])[1]")));
         String originalWindow = driver.getWindowHandle();
-        zaloLink.click();
+        messengerLink.click();
 
         // Chờ và chuyển sang tab mới
         Set<String> windowHandles = driver.getWindowHandles();
@@ -33,9 +38,9 @@ public class Day18_btvn_1 extends CommonBase
         }
 
         // Kiểm tra URL của trang mới
-        String expectedUrl = "https://id.zalo.me/account?continue=http%3A%2F%2Fzalo%2Eme%2F0988169282";
+        String expectedUrl = "https://www.messenger.com/login.php?next=https%3A%2F%2Fwww.messenger.com%2Ft%2F1463299120624512%2F%3Fmessaging_source%3Dsource%253Apages%253Amessage_shortlink%26source_id%3D1441792%26recurring_notification%3D0";
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl, "Zalo link navigation failed!");
-    }
-
+        
+	}
 }
